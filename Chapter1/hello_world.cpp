08c832536b0035ccbf4515d02d8889b7be2d058a
ashuto0sh <azbycs@gmail.com>
2015-10-23 10:16:56 +0530

    Add notes about bufferring

--- Chapter1/hello_world.cpp
+++ Chapter1/hello_world.cpp
@@ -2,6 +2,12 @@
 #include <string>
 
 int main(){
+	/* contents of std::cout are buffered to be flushed 
+	 * only on one out of three ocassions:
+	 * 1. When a std::cin follows
+	 * 2. When buffer is full
+	 * 3. When explicitly told to do so. like writing std::endl
+	 */
 	std::cout<<"Please enter your first name: ";
 
 	std::string name;
