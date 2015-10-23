8a54cca2ad938d15591122c0592f49a4eaa084f5
ashuto0sh <azbycs@gmail.com>
2015-10-23 14:37:39 +0530

    homework program- vectored version

--- Chapter3/homework.cpp
+++ Chapter3/homework.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
 #include <ios>
+#include <vector>
 #include <iomanip>
+#include <algorithm>
 
 using std::cin;
 using std::cout;
@@ -9,6 +11,9 @@ using std::string;
 using std::endl;
 using std::setprecision;
 using std::streamsize;
+using std::vector;
+using std::sort;
+double median(vector<double> vec);
 
 int main(){
 	cout<<"Enter your name: ";
@@ -22,10 +27,10 @@ int main(){
 
 	cout<<"Enter your homework grades followed by end-of-file";
 
-	int cnt=0;
-	double sum=0, x;
+	double x;
+	vector<double> homework;	
 
-	/* Little subtlity. using cin as condition in while loop.
+	/* Little subtleity. using cin as condition in while loop.
 	 * The condition passes if we successfully read x. It fails 
 	 * on one of 3 ocassions:
 	 * 1. Reached end-of-file.
@@ -33,13 +38,28 @@ int main(){
 	 * 3. H/W failure on input device.
 	 */
 	while(cin>>x){
-		sum+=x;
-		cnt++;
+		homework.push_back(x);
 	}
 
 	streamsize prec=cout.precision();
 	//change precision to 3 significant digits
-	cout<<"Your final grade is "<<setprecision(3)<<0.2*mid+0.4*final+0.4*sum/cnt<<setprecision(prec)<<endl;
+	cout<<"Your final grade is "<<setprecision(3)<<0.2*mid+0.4*final+0.4*median(homework)<<endl;
 
 	return 0;
+}
+
+double median(vector<double> vec){
+	typedef vector<double>::size_type vec_sz;
+	vec_sz sz=vec.size();
+
+	if(sz==0){
+		cout<<"Enter your grades "<<endl;
+		return 1;
+	}
+	sort(vec.begin(), vec.end());
+
+	vec_sz mid=sz/2;
+	if(sz%2)
+		return vec[mid];
+	return (vec[mid]+vec[mid-1])/2;
 }
